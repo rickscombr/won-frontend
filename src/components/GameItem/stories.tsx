f6209834c689f728c9cd5823b419e855e5c79c29
Ricardo Marques <rickscombr@gmail.com>
2020-12-28 12:52:00 -0300

    Criação do componente GameItem

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import GameItem, { GameItemProps } from '.'
+
+export default {
+    title: 'GameItem',
+    component: GameItem,
+    args: {
+        img: 'https://source.unsplash.com/user/willianjusten/151x70',
+        title: 'Read Dead Redenption 2',
+        price: 'R$ 215,00'
+    }
+} as Meta
+
+export const Default: Story<GameItemProps> = (args) => <GameItem {...args} />
+
+export const withPaymment: Story<GameItemProps> = (args) => (
+    <GameItem {...args} />
+)
+
+withPaymment.args = {
+    downloadLink: 'https://wongames.com.br/games/nomedojogo',
+    paymentInfo: {
+        number: '**** **** **** 4326',
+        flag: 'mastercard',
+        img: 'img/master-card.png',
+        purchaseDate: 'Purchase mage on 07/20/2020 at 20:32'
+    }
+}
